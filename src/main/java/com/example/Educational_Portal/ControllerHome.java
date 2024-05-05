7819d8374a38164a9d3a19e1343f238bce0da6e3
revoly afanasev <rewoly@mail.ru>
2024-05-05 20:53:08 +0900

    For student

--- src/main/java/com/example/Educational_Portal/ControllerHome.java
+++ src/main/java/com/example/Educational_Portal/ControllerHome.java
@@ -2,11 +2,13 @@ package com.example.Educational_Portal;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+/**
+ * Контроллер - отвечает на запросы пользователей через браузер
+ */
 @Controller
 public class ControllerHome {
     @GetMapping("/")
     private String getInfo() {
-        return "Образовательный портал";
+        return "home_page";
     }
 }
