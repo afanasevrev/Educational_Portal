0e3943d073863004454bca43e91af944bf1fc17e
revoly afanasev <rewoly@mail.ru>
2024-05-05 17:08:25 +0900

    For student

---
+++ src/main/java/com/example/Educational_Portal/EducationalPortalApplication.java
@@ -0,0 +1,10 @@
+package com.example.Educational_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class EducationalPortalApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalPortalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/Educational_Portal/EducationalPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Educational_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
