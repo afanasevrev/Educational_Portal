7001eac377190580e29d8b9f3f8b7b62c23bb23e
unknown <rewoly@mail.ru>
2024-05-06 15:56:31 +0900

    For student

--- src/main/java/com/example/Educational_Portal/ControllerHome.java
+++ src/main/java/com/example/Educational_Portal/ControllerHome.java
@@ -1,7 +1,15 @@
 package com.example.Educational_Portal;
 
+import com.example.Educational_Portal.hibernate.HibernateUtil;
+import com.example.Educational_Portal.hibernate.Materials;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Контроллер - отвечает на запросы пользователей через браузер
  */
@@ -18,5 +26,22 @@ public class ControllerHome {
     @GetMapping("/materials")
     private String getMaterials() {
         return "materials";
+    }
+    /**
+     * Метод возвращает из БД список материалов
+     * @return массив Materials
+     */
+    private synchronized List<Materials> getMaterialsList() {
+        Transaction transaction = null;
+        List<Materials> materials = new ArrayList<>();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            materials = session.createQuery("from Materials", Materials.class).list();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return materials;
     }
 }
