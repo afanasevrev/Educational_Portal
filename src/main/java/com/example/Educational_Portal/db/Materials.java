0f5fbd2e9c798cb95b21715ea50a5666c2ddefd2
unknown <rewoly@mail.ru>
2024-05-07 09:49:13 +0900

    For student

--- src/main/java/com/example/Educational_Portal/db/Materials.java
+++ src/main/java/com/example/Educational_Portal/db/Materials.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 /**
- * Класс сущность для взаимодетйствия с таблице "materials" в БД
+ * Класс сущность для взаимодетйствия с таблицей "materials" в БД
  */
 @Setter
 @Getter
