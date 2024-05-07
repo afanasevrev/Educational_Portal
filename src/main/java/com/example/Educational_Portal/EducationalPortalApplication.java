99c43d0e31b0a19007302cc1ca83696bf7f04640
revoly afanasev <rewoly@mail.ru>
2024-05-08 00:06:47 +0900

    For student

--- src/main/java/com/example/Educational_Portal/EducationalPortalApplication.java
+++ src/main/java/com/example/Educational_Portal/EducationalPortalApplication.java
@@ -2,6 +2,9 @@ package com.example.Educational_Portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Точка запуска Web - сервера
+ */
 @SpringBootApplication
 public class EducationalPortalApplication {
 	public static void main(String[] args) {
