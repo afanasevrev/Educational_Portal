979fd18bebfc9a3e903fe941f849471c6fe72ac8
revoly afanasev <rewoly@mail.ru>
2024-05-08 00:04:22 +0900

    For student

--- src/main/java/com/example/Educational_Portal/ControllerHome.java
+++ src/main/java/com/example/Educational_Portal/ControllerHome.java
@@ -10,7 +10,6 @@ import org.hibernate.Transaction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
