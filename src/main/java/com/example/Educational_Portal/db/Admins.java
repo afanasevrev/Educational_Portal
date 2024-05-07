53a285184e2a23ae51575d9830732c8996624d8c
unknown <rewoly@mail.ru>
2024-05-07 09:40:56 +0900

    For student

--- src/main/java/com/example/Educational_Portal/db/Admins.java
+++ src/main/java/com/example/Educational_Portal/db/Admins.java
@@ -12,5 +12,25 @@ public class Admins {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "patronymic")
+    private String patronymic;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "position")
+    private String position;
+    public Admins(){};
+    public Admins(String surname, String name, String patronymic, String login, String password, String position) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.login = login;
+        this.password = password;
+        this.position = position;
+    }
 }
