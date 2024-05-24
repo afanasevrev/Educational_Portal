ce54b67374bc1a903aed3ebb1333b6ae7995ccc1
unknown <rewoly@mail.ru>
2024-05-24 22:38:57 +0300

    for student

--- src/main/java/com/example/Educational_Portal/EducationalPortalApplication.java
+++ src/main/java/com/example/Educational_Portal/EducationalPortalApplication.java
@@ -1,5 +1,6 @@
 package com.example.Educational_Portal;
 
+import com.example.Educational_Portal.db.Admins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
